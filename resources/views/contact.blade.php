d20aae6d52c467c2e4b28e8ce07be426bd8a9ae5
kurta17 <114169244+kurta17@users.noreply.github.com>
2024-04-19 00:26:59 +0700

    Initial commit

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,5 @@
+<x-site-layout title='Arithmos Academy'>
+
+    <h2 class="text-2xl font-bold">Welcome to the new blog</h2>
+    
+</x-site-layout>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,8 @@
+<x-site-layout title='Arithmos Academy'>
+
+    <h2 class="text-2xl font-bold">Welcome to the new blog</h2>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aliquid amet animi distinctio dolores eius eligendi fugit, illum impedit iusto modi natus obcaecati, pariatur placeat quaerat sed, similique sit temporibus.
+    </p>
+
+</x-site-layout>

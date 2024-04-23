158656708ce343e53b595261579bfb7528e3a9a8
kurta17 <114169244+kurta17@users.noreply.github.com>
2024-04-23 13:47:35 +0700

    added breeze kit

--- resources/views/components/site-layout.blade.php
+++ resources/views/components/site-layout.blade.php
@@ -21,7 +21,7 @@
         <script src="//unpkg.com/alpinejs" defer></script>
 
     </head>
-    <body class="h-full bg-white dark:bg-gray-900">
+    <body class="h-full bg-white">
         <div class="min-h-full">
 
             <x-navigation/>
--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/test', function () {
-    return view('test');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('/contact', function () {
-    return view('contact');
-});
-
-//add a route for questions which connected to the questions view from grade 2 to 11
-Route::get('/question', function () {
-    return view('question');
-});
+require __DIR__.'/auth.php';
